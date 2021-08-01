229776f699159c04535c9a7196bbf920edc37ae8
TalkingDragon <34077525+talkingdragon@users.noreply.github.com>
2021-08-01 15:44:50 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,13 @@
 #Script execute True or False
+
+#Software Update
 UpdateUpgrade = False
-Argon         = False
+
+#Raspberry configs
 SSH           = False
 VNC           = False
-Moonlight     = False
-Firefox       = False
-MariaDB       = False
-PySimpleGui   = False
+Camera        = False
+Overscan      = False #Enable if you see black edges
 Hostname      = False #Reboot forced
 hostname      = "raspberry2
 Overclock     = False
@@ -15,11 +16,24 @@ GPU           = False #GPU memory split
 gpu           = 256 #256, 512, 1024
 Country       = False #Wifi country
 country       = "ES"
+TimeZone      = False
+timezone      = "Europe/Spain" #run "timedatectl list-timezones" for a list
 Keyboard      = False
 keyboard      = "ES"
 Wifi          = False #Adding
 ssid          = "ssid_name" #wifi name
 ssid_pass     = "******" #wifi password
+
+#Software/Programs
+Argon         = False
+Moonlight     = False
+Firefox       = False
+MariaDB       = False #Includes initial setup. Removes test DB and sets new root password
+mariadb_pass  = "****"
+
+#Packages
+PySimpleGui   = False
+
 
 
 import subprocess
